82a2c4e6b1c3d3b27daff1017d09e259b15b1f70
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-08 19:55:08 +0530

    w

--- Missing268.java
+++ Missing268.java
@@ -1,8 +1,11 @@
 
+import java.util.Arrays;
+
+
 
 public class Missing268 {
     public static void main(String[] args) {
-        int[] nums = {9,6,4,2,3,5,8,7,0,1};
+        int[] nums = {9,6};
 
         int i = 0;
         while(i < nums.length){
@@ -18,7 +21,7 @@ public class Missing268 {
             }
         }
     // for(int a : nums){
-            System.out.println();
+            System.out.println(Arrays.toString(nums));
     // }
 
     }
