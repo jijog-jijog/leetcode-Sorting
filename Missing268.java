3052fb86da650b4ed798f83adaf777c0405834ca
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-08 19:48:57 +0530

    Fix formatting in Missing268.java for improved readability

--- Missing268.java
+++ Missing268.java
@@ -6,7 +6,8 @@ public class Missing268 {
 
         int i = 0;
         while(i < nums.length){
-            int correct = nums[i] - 1;
+            int correct = nums[i] - 1; 
+            
             if (nums[i] != nums[correct]) { 
                 int temp = nums[i];
                 nums[i] = nums[correct];
