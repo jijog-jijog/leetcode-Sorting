de8b4222f1d6387465d7f4173534e8ba08d133cf
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-08 15:49:13 +0530

    Refactor Missing268.java to improve code readability and structure

--- Missing268.java
+++ Missing268.java
@@ -1,5 +1,22 @@
+
+import java.util.Arrays;
+
 public class Missing268 {
     public static void main(String[] args) {
-        
+        int[] nums = {9,6,4,2,3,5,7,0,1};
+
+        int i = 0;
+        while(i < nums.length){
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[correct] = temp;
+                temp = nums[correct];
+
+            }else{
+                i++;
+            }
+        }
+    System.out.println(Arrays.toString(nums));
     }
 }
