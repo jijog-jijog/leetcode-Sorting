b388af115e923c356c09ddc7338bd18be7ff2519
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-08 15:51:23 +0530

    d

--- Missing268.java
+++ Missing268.java
@@ -3,15 +3,15 @@ import java.util.Arrays;
 
 public class Missing268 {
     public static void main(String[] args) {
-        int[] nums = {9,6,4,2,3,5,7,0,1};
+        int[] nums = {9,6,4,2,3,5,8,7,0,1};
 
         int i = 0;
         while(i < nums.length){
             int correct = nums[i] - 1;
             if (nums[i] != nums[correct]) {
                 int temp = nums[i];
+                nums[i] = nums[correct];
                 nums[correct] = temp;
-                temp = nums[correct];
 
             }else{
                 i++;
