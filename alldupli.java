76eebf114a1a2a8a24e326971542c46412170374
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-09 12:27:20 +0530

    3

--- alldupli.java
+++ alldupli.java
@@ -1,14 +1,17 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class alldupli {
     public static void main(String[] args) {
-        int[] nums = {4,3,2,7,8,2,3,1};
+        int[] nums = {1};
+        List<Integer> arr = new ArrayList<>();
 
         int i = 0;
         while(i < nums.length){
             int correct = nums[i]-1;
-            if (nums[i] != correct) {
+            if (nums[i] != nums[correct]) {
                 int temp = nums[i];
                 nums[i] = nums[correct];
                 nums[correct] =temp;
@@ -17,5 +20,13 @@ public class alldupli {
             }
         }
         System.out.println(Arrays.toString(nums));
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j+1) {
+                arr.add(nums[j]);
+            }
+            
+        }
+        System.out.println(arr.toString());
     }
 }
---
+++ mismatched.java
@@ -0,0 +1,13 @@
+public class mismatched {
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,4};
+
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                
+            }
+        }
+    }
+}
