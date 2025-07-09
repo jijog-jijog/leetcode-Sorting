2b23d933c8a91b0c4dcd041cca27cce43a67e817
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-09 11:00:51 +0530

    s

--- missingnums.java
+++ missingnums.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class missingnums {
     public static void main(String[] args) {
         int[] nums = {4,3,2,7,8,2,3,1};
+        List<Integer> arr = new ArrayList<>();
 
         int i = 0;
         while (i < nums.length) {
-            int correct = nums[i];
+            int correct = nums[i] - 1;
 
-            if (nums[i] < nums.length && nums[i] != nums[correct]) {
+            if (nums[i] != nums[correct]) {
                 int temp = nums[i];
                 nums[i] = nums[correct];
                 nums[correct] = temp;
@@ -14,5 +19,13 @@ public class missingnums {
                 i++;
             }
         }
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j+1) {
+                arr.add(j + 1);
+            }
+            
+        }
+        System.out.println(arr.toString());
+        System.out.println(Arrays.toString(nums));
     }
 }
