70ffb9b7affd885b0a97ab98306bed8cefea0e00
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-09 12:28:20 +0530

    e

--- mismatched.java
+++ mismatched.java
@@ -6,7 +6,9 @@ public class mismatched {
         while (i < nums.length) {
             int correct = nums[i] - 1;
             if (nums[i] != nums[correct]) {
-                
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
             }
         }
     }
