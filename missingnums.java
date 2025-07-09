5a2da6becbbb96f33e90d82ad0150f6539bd65f0
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-09 10:21:33 +0530

    e

---
+++ missingnums.java
@@ -0,0 +1,14 @@
+public class missingnums {
+    public static void main(String[] args) {
+        int[] nums = {4,3,2,7,8,2,3,1};
+
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i];
+
+            if (nums[i] < nums.length && nums[i] != nums[correct]) {
+                
+            }
+        }
+    }
+}
