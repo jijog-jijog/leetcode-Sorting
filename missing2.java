d4b73b39ee0075bf009ebc7b45e554206334f938
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-09 09:20:51 +0530

    d

---
+++ missing2.java
@@ -0,0 +1,26 @@
+public class missing2 {
+    public static void main(String[] args) {
+        int[] nums = {9,6,4,2,3,5,7,0,1};
+
+        int i =0;
+        while (i < nums.length) {
+            int correct = nums[i];
+             if (nums[i] < nums.length && nums[i] != nums[correct]) {
+            int temp = nums[i];
+            nums[i] = nums[correct];
+             nums[correct] = temp; 
+        }else{
+            i++;
+        }
+        }
+       
+
+        for (int idx = 0; idx < nums.length; idx++) {
+            if (nums[idx] != idx) {
+                System.out.println(idx);
+                return;
+            }
+        }
+        System.out.println(nums.length);
+    }
+}
