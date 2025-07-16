dee13a15ef40767acdffad9d3b0fa62b49ff5e7d
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-16 23:21:16 +0530

    y

--- missing2.java
+++ missing2.java
@@ -1,6 +1,6 @@
 public class missing2 {
     public static void main(String[] args) {
-        int[] nums = { 9 , 6 , 4,2, 3  , 5 , 7,0 ,1};
+        int[] nums = { 9 , 6 , 4, 2, 3  , 5 , 7,0 ,1};
 
         int i =0;
         while (i < nums.length) {
